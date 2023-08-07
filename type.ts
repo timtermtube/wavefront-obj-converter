5237be4f25bd01a8217a5ed38541ea77edcb51d2
timtermtube <timtermtube@gmail.com>
2023-08-07 13:27:35 +0900

    Update type.ts

--- type.ts
+++ type.ts
@@ -1,6 +1,13 @@
-type point = [number, number, number]; // (x, y, z, ?w) 
+/* a point has a corresponding coordination 
+   that has 3 (x, y, z) or 4 (x, y, z, w) value.*/
+type point = [number, number, number];
+
+/* a combination of vertices 
+   composes a shape according to shape's mode.*/
 type vertices = point[];
 
+/* interface 'shape' defines the expressive type of a vertex combination.
+   supports "face", "line".*/
 interface shape {
     type: "face" | "line"
     vertices: vertices
